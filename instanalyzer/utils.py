503d13a3792c4d0ad906cb58a930e36346e42b17
Oidlichtnwoada <42490916+Oidlichtnwoada@users.noreply.github.com>
2022-09-17 15:08:51 +0200

    fixed bug with follower and following counter

--- instanalyzer/utils.py
+++ instanalyzer/utils.py
@@ -1,5 +1,6 @@
 import os
 import time
+
 import tap
 
 from selenium.webdriver import Chrome
@@ -20,11 +21,11 @@ XPATH_CONSTANTS = {'accept_cookies_button': '/html/body/div[4]/div/div/button[1]
                    'open_followers_button': '/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[1]/'
                                             'section/main/div/header/section/ul/li[2]/a/div',
                    'follower_count': '/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/'
-                                     'section/main/div/ul/li[2]/a/div/span',
+                                     'section/main/div/ul/li[2]/a/div',
                    'follower_list': '/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/'
                                     'div[2]/div/div/div[2]/div[1]/div',
                    'following_count': '/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/'
-                                      'section/main/div/ul/li[3]/a/div/span',
+                                      'section/main/div/ul/li[3]/a/div',
                    'following_list': '/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/'
                                      'div[2]/div/div/div[3]/div[1]/div'}
 
@@ -52,7 +53,7 @@ def open_instagram(driver: Chrome) -> None:
     driver.get('https://www.instagram.com')
 
 
-def get_element(driver: Chrome, xpath_constants_key: str, timeout: float = 10) -> WebElement:
+def get_element(driver: Chrome, xpath_constants_key: str, timeout: float = 60) -> WebElement:
     return WebDriverWait(driver, timeout).until(
         expected_conditions.presence_of_element_located((By.XPATH, XPATH_CONSTANTS[xpath_constants_key])))
 
@@ -87,33 +88,37 @@ def get_profile_name_from_list_element(element: WebElement) -> str:
 
 def go_to_profile(driver: Chrome, username: str) -> None:
     driver.get(f'https://www.instagram.com/{username}/')
+    time.sleep(10)
 
 
 def get_follower_and_following_amount(driver: Chrome) -> tuple[int, int]:
-    follower_count = int(get_element(driver, 'follower_count').text)
-    following_count = int(get_element(driver, 'following_count').text)
+    follower_count = int(get_element(driver, 'follower_count').text.split()[0])
+    following_count = int(get_element(driver, 'following_count').text.split()[0])
     return follower_count, following_count
 
 
 def open_followers(driver: Chrome, username: str) -> None:
     driver.get(f'https://www.instagram.com/{username}/followers/')
+    time.sleep(10)
 
 
 def open_following(driver: Chrome, username: str) -> None:
     driver.get(f'https://www.instagram.com/{username}/following/')
+    time.sleep(10)
 
 
 def get_children(element: WebElement) -> list[WebElement]:
     return element.find_elements(By.XPATH, './child::*')
 
 
-def scroll_down_on_element(element: WebElement) -> None:
+def scroll_down_on_element(driver: Chrome, element: WebElement) -> None:
+    x = 1
     pass
 
 
 def get_list_elements(driver: Chrome, expected_elements: int, list_name: str) -> set[str]:
     while len(get_children(get_element(driver, list_name))) != expected_elements:
-        scroll_down_on_element(get_element(driver, list_name))
+        scroll_down_on_element(driver, get_element(driver, list_name))
     return set(map(get_profile_name_from_list_element, get_children(get_element(driver, list_name))))
 
 
