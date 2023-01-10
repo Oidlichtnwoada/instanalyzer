d0a1cfb708a3351a91d21ac407bde2050c173ee7
Oidlichtnwoada <42490916+Oidlichtnwoada@users.noreply.github.com>
2023-01-10 18:40:30 +0100

    finished instanalyzer by simply using instagrapi instead of cypress

--- instanalyzer/run.py
+++ instanalyzer/run.py
@@ -1,38 +1,22 @@
-import json
-
 import utils
+import instagrapi
 
 
 def main() -> None:
-    # get followers and following accounts
     username, password = utils.get_username_and_password()
-    driver = utils.get_driver()
-    utils.open_instagram(driver)
-    utils.accept_cookies(driver)
-    utils.login(driver, username, password)
-    utils.do_not_save_login_information(driver)
-    utils.turn_off_notifications(driver)
-    utils.go_to_profile(driver, username)
-    follower_amount, following_amount = utils.get_follower_and_following_amount(driver)
-    utils.open_followers(driver, username)
-    follower_set = utils.get_followers(driver, follower_amount)
-    utils.open_following(driver, username)
-    following_set = utils.get_following(driver, following_amount)
-    driver.close()
-    # compute the relevant sets
-    not_following_back_set = following_set - follower_set
-    not_followed_back_by_me_set = follower_set - following_set
-    common_core_set = following_set.intersection(follower_set)
-    with open('not_following_back_set.json', 'w') as file:
-        file.write(json.dumps(list(not_following_back_set)))
-    with open('not_followed_back_by_me_set.json', 'w') as file:
-        file.write(json.dumps(list(not_followed_back_by_me_set)))
-    with open('common_core_set.json', 'w') as file:
-        file.write(json.dumps(list(common_core_set)))
-    print(f'not_following_back_set: {not_following_back_set}')
-    print(f'not_followed_back_by_me_set: {not_followed_back_by_me_set}')
-    print(f'common_core_set: {common_core_set}')
-    print('Thanks for using instanalyzer!')
+    client = instagrapi.Client()
+    client.login(username, password, relogin=True)
+    user_id = client.user_id_from_username(username)
+    followers = client.user_followers_v1(user_id)
+    following = client.user_following_v1(user_id)
+    follower_username_set = set(map(utils.get_username, followers))
+    following_username_set = set(map(utils.get_username, following))
+    common_core = following_username_set.intersection(follower_username_set)
+    not_following_back = following_username_set - follower_username_set
+    not_followed_back = follower_username_set - following_username_set
+    print(f"not following back: {not_following_back}")
+    print(f"not followed back: {not_followed_back}")
+    print(f"common core: {common_core}")
 
 
 if __name__ == '__main__':
--- instanalyzer/utils.py
+++ instanalyzer/utils.py
@@ -1,33 +1,6 @@
-import os
-import time
-
+import instagrapi.types
 import tap
-
-from selenium.webdriver import Chrome
-from selenium.webdriver.common.by import By
-from selenium.webdriver.remote.webelement import WebElement
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions
-
-XPATH_CONSTANTS = {'accept_cookies_button': '/html/body/div[4]/div/div/button[1]',
-                   'username_input': '/html/body/div[1]/section/main/article/div[2]/'
-                                     'div[1]/div[2]/form/div/div[1]/div/label/input',
-                   'password_input': '/html/body/div[1]/section/main/article/div[2]/'
-                                     'div[1]/div[2]/form/div/div[2]/div/label/input',
-                   'login_button': '/html/body/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[3]',
-                   'do_not_save_login_information_button': '/html/body/div[1]/section/main/div/div/div/div/button',
-                   'turn_off_notifications_button': '/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div/div[2]/'
-                                                    'div/div/div/div/div[2]/div/div/div[3]/button[2]',
-                   'open_followers_button': '/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[1]/'
-                                            'section/main/div/header/section/ul/li[2]/a/div',
-                   'follower_count': '/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/'
-                                     'section/main/div/ul/li[2]/a/div',
-                   'follower_list': '/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/'
-                                    'div[2]/div/div/div[2]/div[1]/div',
-                   'following_count': '/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/'
-                                      'section/main/div/ul/li[3]/a/div',
-                   'following_list': '/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/'
-                                     'div[2]/div/div/div[3]/div[1]/div'}
+import os
 
 
 class ScriptArgumentParser(tap.Tap):
@@ -40,91 +13,5 @@ def get_username_and_password() -> tuple[str, str]:
     return args.username, args.password
 
 
-def get_driver() -> Chrome:
-    return Chrome()
-
-
-def send_keys_with_wait(element: WebElement, text: str, wait_time: float = 1) -> None:
-    element.send_keys(text)
-    time.sleep(wait_time)
-
-
-def open_instagram(driver: Chrome) -> None:
-    driver.get('https://www.instagram.com')
-
-
-def get_element(driver: Chrome, xpath_constants_key: str, timeout: float = 60) -> WebElement:
-    return WebDriverWait(driver, timeout).until(
-        expected_conditions.presence_of_element_located((By.XPATH, XPATH_CONSTANTS[xpath_constants_key])))
-
-
-def accept_cookies(driver: Chrome) -> None:
-    accept_cookies_button = get_element(driver, 'accept_cookies_button')
-    accept_cookies_button.click()
-
-
-def login(driver: Chrome, username: str, password: str) -> None:
-    username_input = get_element(driver, 'username_input')
-    send_keys_with_wait(username_input, username)
-    password_input = get_element(driver, 'password_input')
-    send_keys_with_wait(password_input, password)
-    login_button = get_element(driver, 'login_button')
-    login_button.click()
-
-
-def do_not_save_login_information(driver: Chrome) -> None:
-    do_not_save_login_information_button = get_element(driver, 'do_not_save_login_information_button')
-    do_not_save_login_information_button.click()
-
-
-def turn_off_notifications(driver: Chrome) -> None:
-    turn_off_notifications_button = get_element(driver, 'turn_off_notifications_button')
-    turn_off_notifications_button.click()
-
-
-def get_profile_name_from_list_element(element: WebElement) -> str:
-    return element.text.split()[0]
-
-
-def go_to_profile(driver: Chrome, username: str) -> None:
-    driver.get(f'https://www.instagram.com/{username}/')
-    time.sleep(10)
-
-
-def get_follower_and_following_amount(driver: Chrome) -> tuple[int, int]:
-    follower_count = int(get_element(driver, 'follower_count').text.split()[0])
-    following_count = int(get_element(driver, 'following_count').text.split()[0])
-    return follower_count, following_count
-
-
-def open_followers(driver: Chrome, username: str) -> None:
-    driver.get(f'https://www.instagram.com/{username}/followers/')
-    time.sleep(10)
-
-
-def open_following(driver: Chrome, username: str) -> None:
-    driver.get(f'https://www.instagram.com/{username}/following/')
-    time.sleep(10)
-
-
-def get_children(element: WebElement) -> list[WebElement]:
-    return element.find_elements(By.XPATH, './child::*')
-
-
-def scroll_down_on_element(driver: Chrome, element: WebElement) -> None:
-    x = 1
-    pass
-
-
-def get_list_elements(driver: Chrome, expected_elements: int, list_name: str) -> set[str]:
-    while len(get_children(get_element(driver, list_name))) != expected_elements:
-        scroll_down_on_element(driver, get_element(driver, list_name))
-    return set(map(get_profile_name_from_list_element, get_children(get_element(driver, list_name))))
-
-
-def get_followers(driver: Chrome, follower_amount: int) -> set[str]:
-    return get_list_elements(driver, follower_amount, 'follower_list')
-
-
-def get_following(driver: Chrome, following_amount: int) -> set[str]:
-    return get_list_elements(driver, following_amount, 'following_list')
+def get_username(user: instagrapi.types.UserShort) -> str:
+    return user.username
